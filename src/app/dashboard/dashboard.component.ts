a3b057d0be90a1ec11e3c8d71f5616bba9d8519b
Michael <mikeynon@gmail.com>
2018-04-08 14:27:12 -0700

    component dashboard routed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,9 +15,8 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-
   getHeroes(): void {
-  this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
 }
 }
