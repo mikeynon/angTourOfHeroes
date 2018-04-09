78fed9026ebf7894f381abf3ca515a9215df25db
Michael <mikeynon@gmail.com>
2018-04-09 00:38:01 -0700

    cleaned up heroes TS

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,25 +10,17 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-
-  selectedHero: Hero;
-
 
   constructor(private heroService: HeroService) { }
 
-  getHeroes(): void {
-  this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-}
   ngOnInit() {
-  this.getHeroes();
-}
+    this.getHeroes();
+  }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
-
 
